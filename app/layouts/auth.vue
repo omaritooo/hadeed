6aca7f74e736ed40181a290649d0cb5d9234fa59
omaritooo <themdic227@gmail.com>
2026-08-26 14:51:18 +0300

    feat: add auth layout for login/signup-adjacent pages

---
+++ app/layouts/auth.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="flex min-h-dvh flex-col justify-center bg-linear-to-b from-background from-0% via-primary/6 via-30% to-background to-70% px-5 py-5">
+    <NuxtPage />
+  </main>
+</template>
